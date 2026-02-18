1a1dac33e829949e31a50857f75b57f4597115ae
lip47 <jonasvivier@gmail.com>
2026-02-18 10:28:15 +0000

    étape 2 : get all users

---
+++ get_all_users.php
@@ -0,0 +1,11 @@
+<?php
+    require_once("connexion.php");
+    
+    $query = $db->prepare('SELECT * FROM users');
+    $query->execute();
+    $users = $query->fetchAll(PDO::FETCH_ASSOC);    
+    
+    var_dump($users);
+    
+
+?>
