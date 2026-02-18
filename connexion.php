58d1ba2a4e442662678feb4b36b522310078929d
lip47 <jonasvivier@gmail.com>
2026-02-18 09:34:08 +0000

    étape 1 : connexion

---
+++ connexion.php
@@ -0,0 +1,19 @@
+<?php
+    $host = "db.3wa.io";
+    $port = "3306";
+    $dbname = "jonasvivier_phpj6";
+    $connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+    
+    $user = "jonasvivier";
+    $password = "e00a941c2593ab6705b70c1d7b8083ea";
+    
+
+    $db = new PDO(
+        $connexionString,
+        $user,
+        $password
+    );
+    
+    var_dump($db);
+
+?>
